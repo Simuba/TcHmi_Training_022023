7e12d197a102536d7bd04a6fe688d346e98aef0d
Simonba <s.bachmann@beckhoff.ch>
2023-03-02 17:38:13 +0100

    commit end day3

---
+++ AssemblyLine_Training/Hmi/Functions/FunctionJS1.js
@@ -0,0 +1,15 @@
+﻿// Keep these lines for a best effort IntelliSense of Visual Studio 2017 and higher.
+/// <reference path="./../../Packages/Beckhoff.TwinCAT.HMI.Framework.12.758.8/runtimes/native1.12-tchmi/TcHmi.d.ts" />
+
+(function (/** @type {globalThis.TcHmi} */ TcHmi) {
+    var Functions;
+    (function (/** @type {globalThis.TcHmi.Functions} */ Functions) {
+        var Hmi;
+        (function (Hmi) {
+            function FunctionJS1(par1) {
+            }
+            Hmi.FunctionJS1 = FunctionJS1;
+        })(Hmi = Functions.Hmi || (Functions.Hmi = {}));
+    })(Functions = TcHmi.Functions || (TcHmi.Functions = {}));
+})(TcHmi);
+TcHmi.Functions.registerFunctionEx('FunctionJS1', 'TcHmi.Functions.Hmi', TcHmi.Functions.Hmi.FunctionJS1);
---
+++ AssemblyLine_Training/Hmi/Functions/fetchData.js
@@ -0,0 +1,27 @@
+﻿// Keep these lines for a best effort IntelliSense of Visual Studio 2017 and higher.
+/// <reference path="./../../Packages/Beckhoff.TwinCAT.HMI.Framework.12.758.8/runtimes/native1.12-tchmi/TcHmi.d.ts" />
+
+(function (/** @type {globalThis.TcHmi} */ TcHmi) {
+    var Functions;
+    (function (/** @type {globalThis.TcHmi.Functions} */ Functions) {
+        var Hmi;
+        (function (Hmi) {
+            function fetchData(par1) {
+                let count = (Math.random() * 10) +2;
+                fetch('https://random-data-api.com/api/v2/beers/?size=' + count)
+                    .then((response) => response.json())
+                    .then((data) => {
+                        TcHmi.Symbol.writeEx('%i%Data%/i%', data, function (data) {
+                            if (data.error === TcHmi.Errors.NONE) {
+                                // Handle success... 
+                            } else {
+                                // Handle error... 
+                            }
+                        });
+                    });       
+            }
+            Hmi.fetchData = fetchData;
+        })(Hmi = Functions.Hmi || (Functions.Hmi = {}));
+    })(Functions = TcHmi.Functions || (TcHmi.Functions = {}));
+})(TcHmi);
+TcHmi.Functions.registerFunctionEx('fetchData', 'TcHmi.Functions.Hmi', TcHmi.Functions.Hmi.fetchData);
